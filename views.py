eaad9067e122f906b7faf7f69bb8fbf85608e318
Mkamal-45 <mohammadaniskamal@gmail.com>
2026-04-11 16:41:00 -0230

    created the collection route and collection template, added the url for collections on basehtml

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 views_blueprint = Blueprint('views', __name__)
 
 @views_blueprint.route('/')
 def home():
     return render_template('home.html')
+
+@views_blueprint.route('/collection')
+def collection():
+    return render_template('collection.html')
