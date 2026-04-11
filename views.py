b89a06f95ca59d8639deb583b8d0d5d822b94d1b
Mkamal-45 <mohammadaniskamal@gmail.com>
2026-04-11 17:21:28 -0230

    Updated the collection html to give base structure, created the method for collection with a test deck

--- views.py
+++ views.py
@@ -8,4 +8,29 @@ def home():
 
 @views_blueprint.route('/collection')
 def collection():
-    return render_template('collection.html')
+    if 'cards' not in session:
+        session['cards']=[
+            {
+                'id': 1,
+                'name': 'Scizor',
+                'hp': '140',
+                'type': 'Steel',
+                'image': 'https://assets.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SV03/SV03_EN_205.png'
+            },
+            {
+                'id': 2,
+                'name': 'Nidoking',
+                'hp': '330',
+                'type': 'Dark',
+                'image': 'https://assets.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SV10/SV10_EN_233.png'
+
+            },
+            {
+                'id': 3,
+                'name': 'Blaziken',
+                'hp': '170',
+                'type': 'Fire',
+                'image': 'https://assets.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SWSH3/SWSH3_EN_24.png'
+            }
+        ]
+    return render_template('collection.html', cards=session['cards'])
