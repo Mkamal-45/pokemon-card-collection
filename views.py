05378eaef46691c7b58c0e7d5053af05339f13ad
Mkamal-45 <mohammadaniskamal@gmail.com>
2026-04-13 21:38:43 -0230

    added logic to the add_card function and tested it, need to work on picture size because its not as I planned to look like

--- views.py
+++ views.py
@@ -1,6 +1,12 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request
+from werkzeug.utils import secure_filename
+import os
 
 views_blueprint = Blueprint('views', __name__)
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @views_blueprint.route('/')
 def home():
@@ -37,8 +43,31 @@ def collection():
 
 @views_blueprint.route('/add', methods=['GET', 'POST'])
 def add_card():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        hp = request.form.get('hp')
+        card_type = request.form.get('type')
+        file = request.files.get('image')
+        if not name or not hp or not card_type or not file:
+            return "Error: All fields are required!", 400
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+            filepath = os.path.join(UPLOAD_FOLDER, filename)
+            file.save(filepath)
+            new_card = {
+                'id': len(session.get('cards', [])) + 1,
+                'name': name,
+                'hp': hp,
+                'type': card_type,
+                'image': '/' + filepath.replace('\\', '/')
+            }
+            session['cards'].append(new_card)
+            session.modified = True
+            return redirect(url_for('views.collection'))
+        else:
+            return "Error: Invalid file type. Only images allowed.", 400
     return render_template('add_card.html')
-
 
 @views_blueprint.route('/remove/<int:card_id>', methods=['POST'])
 def remove_card(card_id):
